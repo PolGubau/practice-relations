854e29c6c16420c09c001c3ea6a251ec2d34a4b9
Pol Gubau Amores <gubaupol@gmail.com>
2024-10-11 22:01:53 +0200

    first commit

---
+++ src/main/java/com/polgubau/udemy/relations/practice_relationcs/PracticeRelationcsApplication.java
@@ -0,0 +1,13 @@
+package com.polgubau.udemy.relations.practice_relationcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRelationcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRelationcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polgubau/udemy/relations/practice_relationcs/PracticeRelationcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polgubau.udemy.relations.practice_relationcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeRelationcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
